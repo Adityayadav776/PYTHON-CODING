8f7b8b0af084851eac7b9706e57b89d03df5b92c
Aditya Yadav <yadavadityayt@gmail.com>
2025-07-26 09:48:51 +0530

    Create 2.4 problem.py

---
+++ 2.4 problem.py
@@ -0,0 +1,7 @@
+#Write a program to calculate the square of a number entered by the user.
+n=float(input("Enter the number the find square of:"))
+print("Square of",n,":",n*n)  # or n**2 is also valid and better as it gives free hand to increase the power in future if needed.
+#Example of using power operator
+print("Square of",n,":",n**2)
+print("Cube of",n,":",n**3)
+print("Fourth power of",n,":",n**4)
